6425256ea5f7d78c56660c67fffb3f3c415c2d03
David Johnson <dave@g4dpz.me.uk>
2023-05-15 14:01:44 +0100

    Added GPGAA string

--- gpsusb.c
+++ gpsusb.c
@@ -37,6 +37,55 @@ void sendUSB(char* latLongAz) {
     serialPuts(fd, latLongAz);
 }
 
+char* nmeaTimestamp(char *datetime) {
+    int i = 0;
+
+    char tokens[3][64] = {"","",""};
+
+   char* token = strtok(datetime, ":");
+   while (token != NULL) {
+	tokens[i] = token;
+        i++;
+        token = strtok(NULL, ":");
+   }
+   char* timeStamp = malloc(64);
+   sprintf(timeStamp, "%2d%2d%2d.00,\0",token[0],token[1],token[2]);
+   return timeStamp;
+}
+
+int sendNmeaUsb(received_t *t) {
+
+    int i = 0;
+
+    char tokens[20][64] = {"","","","","","","","","","","","","","","","","","","",""};
+
+    char* token = strtok(t->Message, ",");
+
+    while (token != NULL) {
+	tokens[i] = token;
+        i++;
+        token = strtok(NULL, ",");
+    }
+
+    // we are generating the format:
+    // $GPGGA,181908.00,3404.7041778,N,07044.3966270,W,4,13,1.00,495.144,M,29.200,M,0.10,0000*40
+
+    char nmeaString[256] = "";
+    strcat(nmeaString, "$GPGGA,");
+    strcat(nmeaString, nmeaTimestamp(tokens[0]); // 181908.00
+    strcat(nmeaString, nmeaLatitude(tokens[3])); // 3404.7041778,N
+    strcat(nmeaString, nmeaLongitude(tokens[4]));// 07044.3966270,W
+    strcat(nmeaString, "4,");                    // Quality
+    strcat(nmeaString, "13,");                   // Number of satllites
+    strcat(nmeaString, "1.00,");                 // HDOP
+    strcat(nmeaString, nmeaAltitude(tokens[5])); // 495.144,M
+    strcat(nmeaString, "0.0,M,");  // Geoidal Separation
+    strcat(nmeaString, "0,");            // Age
+    strcat(nmeaString, "0,");    // Correction
+    strcat(nmeaString, genCRC(nmeaString));
+    sendUSB(nmeaString);
+}`
+
 int sendGpsUsb(received_t *t ) {
     char* token = strtok(t->Message, ",");
     char gpsStr[64] = "";
